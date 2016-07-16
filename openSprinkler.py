d7c1e38cf5ad7e6f22dd7a366b26732a53fbb78b
devanl <devan@lippman.net>
2016-07-15 21:57:19 -0400

    Change Controller Variables implementation

--- openSprinkler.py
+++ openSprinkler.py
@@ -3,6 +3,8 @@ from hashlib import md5
 import logging
 import colorlog
 
+STATUS_SUCCESS = 1
+
 STATUS_CODES = {1:'Success',
                 2:'Unauthorized (e.g. missing password or password is incorrect)',
                 3:'Mismatch (e.g. new password and confirmation password do not match)',
@@ -15,14 +17,24 @@ STATUS_CODES = {1:'Success',
 
 class OpenSprinkler:
   class CV:
-    def __init__(self, parent):
-      self.parent = parent
-
     my_args = ['rsn', 'rbt', 'en', 'rd', 're']
+    my_longhand = {'reset_all':'rsn',
+                   'reboot':'rbt',
+                   'enable':'en',
+                   'rain_delay':'rd',
+                   'remote_extension':'re'}
+
+    def __init__(self, p):
+      self.parent = p
+      self.my_args.extend(self.my_longhand.keys())
 
     def __setattr__(self, name, value):
       if name in self.my_args:
-        self.parent._json_get('cv', {name:value}
+        if name in self.my_longhand.keys():
+          name = self.my_longhand[name]
+        self.parent._json_get('cv', {name:value})
+      else:
+        super().__setattr__(name, value)
 
   def __init__(self, hostname, password, log=None):
     if log is None:
@@ -37,24 +49,27 @@ class OpenSprinkler:
     self.cv = self.CV(self)
 
   def _json_get(self, path, variables=None):
-    requests_str = 'http://' +
-                   self.hostname +
-                   '/' +
-                   path +
-                   '?pw=' +
-                   self.password
+    requests_str = "http://%s/%s/?pw=%s" % (self.hostname, 
+                                            path,
+                                            self.password)
 
     if variables:
-      for k,v in variables.iteritems():
-        requests_str += '&' + k + '=' + v
+      for k,v in variables.items():
+        requests_str += '&' + str(k) + '=' + str(v)
 
     r = requests.get(requests_str) 
 
-    self.log.debug('GET status: %d', r.status_code)
+    self.log.debug('GET %s status: %d', requests_str, r.status_code)
     if r.status_code != 200:
       raise ValueError('Failed GET request with status %d.', r.status_code)
 
-    return r.json()
+    retval = r.json()
+    if 'result' in retval and retval['result'] != STATUS_SUCCESS:
+      raise ValueError('Failure response (%d):%s', 
+                       retval['result'], 
+                       STATUS_CODES[retval['result']])
+
+    return retval
 
   @property
   def jc(self):
@@ -82,3 +97,14 @@ if __name__ == "__main__":
   os_device = OpenSprinkler(hostname, password, log=log)
 
   print(os_device.jc)
+
+  log.info('Setting rain delay for 1 hour')
+  os_device.cv.rain_delay = 1
+
+  print(os_device.jc)
+
+  log.info('Setting rain delay to 0')
+  os_device.cv.rain_delay = 0
+
+  print(os_device.jc)
+
